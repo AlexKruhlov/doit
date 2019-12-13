22e7e3eb67ba1fe6bc146c34122872315fd0ef99
Oleksandr Kruhlov <alkruglov777@gmail.com>
2019-12-14 01:50:21 +0200

    Create application, add README.md

---
+++ src/main/java/ua/com/rafael/doit/DoitApplication.java
@@ -0,0 +1,12 @@
+package ua.com.rafael.doit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoitApplication.class, args);
+    }
+}
